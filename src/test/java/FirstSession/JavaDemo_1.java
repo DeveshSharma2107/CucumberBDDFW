996653f971cc09f8f076ce12e1f7c67613ce498c
sarangholey <sarangholey@gmail.com>
2023-05-20 11:08:37 +0530

    Remaned the Java file with name error and Removed empty spaces form POM.xml file

--- src/test/java/FirstSession/JavaDemo_1.java
+++ src/test/java/FirstSession/JavaDemo_1.java
@@ -1,9 +1,10 @@
 package FirstSession;
 
-public class JavaDemo {
+public class JavaDemo_1 {
 	
 	public static void main(String[] args) 
 	{
+		System.out.println("Hay there I am writing my first java code line=");
 		System.out.println("Hay there I am writing my first java code line=");
 	}
 
