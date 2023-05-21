13b8d31947af20949fb92e9d2a7aef1ab0c10023
sarangholey <sarangholey@gmail.com>
2023-05-21 11:03:26 +0530

    Made the changes as asked by Devesh

--- src/test/java/FirstSession/Feature.java
+++ src/test/java/FirstSession/Feature.java
@@ -5,13 +5,9 @@ public class Feature {
 	public static void main(String[] args) {
 		
 		System.out.println("Given");
-		System.out.println("Given");
+		System.out.println("And");
 		System.out.println("When");
-		System.out.println("Then");
-		
-		
-		
-		
+		System.out.println("Then");	
 	}
 
 }
