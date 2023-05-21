965c9cab9f4e2a0e0ed2c614746a3459d3b5f0dc
sarangholey <sarangholey@gmail.com>
2023-05-21 10:55:48 +0530

    Added feature file of no 32

---
+++ src/test/java/FirstSession/Feature.java
@@ -0,0 +1,17 @@
+package FirstSession;
+
+public class Feature {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Given");
+		System.out.println("Given");
+		System.out.println("When");
+		System.out.println("Then");
+		
+		
+		
+		
+	}
+
+}
